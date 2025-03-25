e5b2f29a56a953c342eb09c7f6b0a29c7f4ff4e7
Dereck Mezquita <dereck@mezquita.io>
2025-03-25 01:05:41 -0500

    Cleaned!

--- index.js
+++ index.js
@@ -1,50 +1,76 @@
-const fs = require('fs');
+import { readFileSync } from 'fs';
 
-// Code for getting time stamp
-Object.defineProperty(Date.prototype, 'YYYYMMDDHHMMSS', {
-    value: function () {
-        function pad2(n) {
-            // always returns a string
-            return (n < 10 ? '0' : '') + n;
-        }
-
-        return (
-            this.getFullYear() +
-            pad2(this.getMonth() + 1) +
-            pad2(this.getDate()) +
-            '-' +
-            pad2(this.getHours()) +
-            pad2(this.getMinutes()) +
-            pad2(this.getSeconds())
-        );
-    }
-});
-
-const ymdt = new Date().YYYYMMDDHHMMSS();
-
-// console.log(ymdt);
-
+// if you want to change the name of a user based on the ID
 const users = {
     5210555339: 'Liza', // set unique ID number and name
     4656883065: 'Dereck'
 };
 
-const messages = JSON.parse(fs.readFileSync('./data/result.json').toString())[
-    'messages'
-];
+const input_path = './data/result.json';
+const output_path = `./outputs/${generate_timestamp()}_output.txt`;
 
-let result = '';
+const whatsapp_data = convert_telegram_to_whatsapp(input_path, users);
+console.log(whatsapp_data);
+// fs.writeFileSync(output_path, whatsapp_data);
 
-for (let msg of messages) {
+function convert_telegram_to_whatsapp(input_path, users_map) {
+    const messages = JSON.parse(readFileSync(input_path).toString())[
+        'messages'
+    ];
+
+    let result = '';
+
+    for (let msg of messages) {
+        // const edited_date = {};
+        // let edited = true;
+        const from_id = msg['from_id'];
+        let from_name = msg['from'];
+
+        if (from_id in users_map) {
+            from_name = users_map[from_id];
+        }
+
+        const media_type = msg['media_type'];
+        const duration = msg['duration_seconds'];
+        const text = msg['text'];
+
+        const date = parse_tg_datetime(msg['date']);
+
+        if (media_type == 'voice_message') {
+            // result += `${date.date} ${date.time}, voice, ${duration}, ${users[from_id]}, NA, NA\n`;
+            result += `[${date.month}/${date.day}/${date.year}, ${date.time}] ${users_map[from_id]}:\n`;
+            continue;
+        }
+
+        if (text !== '' && typeof text == 'string') {
+            // result += `${date.date} ${date.time}, text, NA, ${users_map[from_id]}, ${(edited ? `${edited_date.date} ${edited_date.time}` : "NA")}, ${text.replace(/\n/gi, "\\n")}\n`;
+            result += `[${date.month}/${date.day}/${date.year}, ${date.time}] ${users_map[from_id]}: ${text.replace(/\n/gi, '\\n')}\n`;
+        }
+    }
+
+    return result;
+    // [YYYY-MM-DD HH:MM:SS] SENDER: MESSAGE
+    // [2020-04-07 14:14:37] user:   This is a first message
+}
+
+function generate_timestamp() {
+    const now = new Date();
+
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // +1 because months are 0-based
+    const day = String(now.getDate()).padStart(2, '0');
+
+    // Get time components
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    return `${year}${month}${day}-${hours}${minutes}${seconds}`;
+}
+
+function parse_tg_datetime(tg_dt) {
     const date = {};
-    // const edited_date = {};
-    // let edited = true;
-    const from_id = msg['from_id'];
-    const media_type = msg['media_type'];
-    const duration = msg['duration_seconds'];
-    const text = msg['text'];
-
-    const date_parsed = msg['date'].split('T'); // "date": "2020-01-24T18:22:26"
+    const date_parsed = tg_dt.split('T'); // "date": "2020-01-24T18:22:26"
     date.date = date_parsed[0];
     date.time = date_parsed[1];
 
@@ -53,30 +79,5 @@ for (let msg of messages) {
     date.month = date_parts[1].replace(/^0+/, '');
     date.day = date_parts[2].replace(/^0+/, '');
 
-    // const edited_date_parsed = msg['edited'].split('T');
-    // edited_date.date = edited_date_parsed[0];
-    // edited_date.time = edited_date_parsed[1];
-
-    // console.log(edited_date_parsed);
-    // console.log(edited_date_parsed.length);
-
-    // if(edited_date.date.substr(0, 4) == "1969") {
-    //     edited = false;
-    // }
-
-    if (media_type == 'voice_message') {
-        // result += `${date.date} ${date.time}, voice, ${duration}, ${users[from_id]}, NA, NA\n`;
-        result += `[${date.month}/${date.day}/${date.year}, ${date.time}] ${users[from_id]}:\n`;
-        continue;
-    }
-
-    if (text !== '' && typeof text == 'string') {
-        // result += `${date.date} ${date.time}, text, NA, ${users[from_id]}, ${(edited ? `${edited_date.date} ${edited_date.time}` : "NA")}, ${text.replace(/\n/gi, "\\n")}\n`;
-        result += `[${date.month}/${date.day}/${date.year}, ${date.time}] ${users[from_id]}: ${text.replace(/\n/gi, '\\n')}\n`;
-    }
+    return date;
 }
-
-fs.writeFileSync(`./outputs/${ymdt}_output.txt`, result);
-
-// [YYYY-MM-DD HH:MM:SS] SENDER: MESSAGE
-// [2020-04-07 14:14:37] user:   This is a first message
